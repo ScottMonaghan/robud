61e63d327fbc61e5000e189c67c77ca0e45e8eaf
Scott Monaghan <scott.monaghan@gmail.com>
2021-09-24 21:33:50 -0500

    Added logging to robud_voice

--- robud_voice/robud_voice.py
+++ robud_voice/robud_voice.py
@@ -1,20 +1,61 @@
 import paho.mqtt.client as mqtt
 import subprocess
+import random
+from robud.robud_logging.MQTTHandler import MQTTHandler
+import argparse
+import logging
+from datetime import datetime
+import os
+import sys
+import traceback
 
-MQTT_BROKER_ADDRESS = "localhost"
-MQTT_CLIENT_NAME = "robud_voice.py"
+random.seed()
+
+MQTT_BROKER_ADDRESS = "robud.local"
+MQTT_CLIENT_NAME = "robud_voice.py" + str(random.randint(0,999999999))
 TOPIC_ROBUD_VOICE_TEXT_INPUT = 'robud/robud_voice/text_input'
 
-def on_message_robud_voice_text_input(client, userdata, message):
-    tts = message.payload.decode()
-    print (tts)
-    subprocess.Popen('espeak-ng -m -v us-mbrola-1 -s 140 "'+ tts +'" --stdout | aplay -Dplug:ladspa -', shell=True)
+TOPIC_ROBUD_LOGGING_LOG = "robud/robud_logging/log"
+TOPIC_ROBUD_LOGGING_LOG_SIGNED = TOPIC_ROBUD_LOGGING_LOG + "/" + MQTT_CLIENT_NAME
+TOPIC_ROBUD_LOGGING_LOG_ALL = TOPIC_ROBUD_LOGGING_LOG + "/#"
+LOGGING_LEVEL = logging.DEBUG
 
-client_userdata = {}
-mqtt_client = mqtt.Client(client_id=MQTT_CLIENT_NAME,userdata=client_userdata)
-mqtt_client.connect(MQTT_BROKER_ADDRESS)
-print('MQTT Client Connected')
-mqtt_client.subscribe(TOPIC_ROBUD_VOICE_TEXT_INPUT)
-mqtt_client.message_callback_add(TOPIC_ROBUD_VOICE_TEXT_INPUT,on_message_robud_voice_text_input)
-print('Waiting for messages...')
-mqtt_client.loop_forever()
+#parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-o", "--Output", help="Log Ouput Prefix", default="logs/robud_voice_log_")
+args = parser.parse_args()
+
+#initialize logger
+logger=logging.getLogger()
+file_path = args.Output + datetime.now().strftime("%Y-%m-%d") + ".txt"
+directory = os.path.dirname(file_path)
+if not os.path.exists(directory):
+    os.makedirs(directory)
+log_file = open(file_path, "a")
+myHandler = MQTTHandler(hostname=MQTT_BROKER_ADDRESS, topic=TOPIC_ROBUD_LOGGING_LOG_SIGNED, qos=2, log_file=log_file)
+myHandler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s: %(filename)s: %(message)s'))
+logger.addHandler(myHandler)
+logger.level = LOGGING_LEVEL
+
+try:
+    def on_message_robud_voice_text_input(client, userdata, message):
+        tts = message.payload.decode()
+        logger.debug(tts)
+        subprocess.Popen('espeak-ng -m -v us-mbrola-1 -s 140 "'+ tts +'" --stdout | aplay -Dplug:ladspa -', shell=True)
+
+    client_userdata = {}
+    mqtt_client = mqtt.Client(client_id=MQTT_CLIENT_NAME,userdata=client_userdata)
+    mqtt_client.connect(MQTT_BROKER_ADDRESS)
+    logger.info('MQTT Client Connected')
+    mqtt_client.subscribe(TOPIC_ROBUD_VOICE_TEXT_INPUT)
+    mqtt_client.message_callback_add(TOPIC_ROBUD_VOICE_TEXT_INPUT,on_message_robud_voice_text_input)
+    logger.info('Waiting for messages...')
+    mqtt_client.loop_forever()
+except Exception as e:
+    logger.critical(str(e) + "\n" + traceback.format_exc())
+except KeyboardInterrupt:
+    logger.info("Exited with Keyboard Interrupt")
+    try:
+        sys.exit(0)
+    except SystemExit:
+        os._exit(0)
